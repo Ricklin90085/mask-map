d1670e2a5a5c7def28e8bbdabf253f85bd15c9c9
ricklin90085 <boylin90085@gmail.com>
2020-02-05 23:35:42 +0800

    Hello World

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import storesData from './assets/stores.json'
+export default {
+  name: 'app',
+  data () {
+    return {
+      storesData
+    }
+  },
+  mounted () {
+    console.log(this.json)
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
